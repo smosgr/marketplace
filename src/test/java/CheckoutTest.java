55f85b2cd053785b300de4910797fa0a4286d980
smosgr <stel.moschos@gmail.com>
2019-10-20 14:42:34 +0100

    initial commit

---
+++ src/test/java/CheckoutTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+public class CheckoutTest {
+
+    /*
+     *
+     * 1. Checkout single product
+     * 2. Checkout 001, 002, 003
+     * 3. Checkout 002, 003 and apply promotion (over £60)
+     * 4. Checkout 001, 001 and apply promotion (2-4-minus10%) and print total
+     *
+     */
+
+    @Test
+    public void test() {
+        assert true;
+    }
+}
